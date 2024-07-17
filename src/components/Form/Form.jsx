c97953cb32b49f4c1f64e6b5aa07e11de2790cea
SergiiOst <sergeyostapenko85@gmail.com>
2024-07-18 01:58:11 +0300

    commit

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,47 +1,77 @@
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 const MyForm = () => {
+  const schema = Yup.object({
+    name: Yup.string()
+      .required("This field is required!")
+      .min(3, "name mus be more than 3 chars!")
+      .max(12, "Name must be less than 12 chars!"),
+    surname: Yup.string()
+      .required("This field is required!")
+      .min(3, "Surname mus be more than 3 chars!")
+      .max(12, "Surname must be less than 12 chars!"),
+    age: Yup.number()
+      .required("This field is required!")
+      .min(3, "age mus be more than 3 years!")
+      .max(99, "You are too old !"),
+    role: Yup.string().oneOf(["admin", "user"]),
+    email: Yup.string().email("Email is not valid"),
+  });
   const initialValues = {
     name: "",
     surname: "",
     age: "",
     email: "",
+    role: "user",
+    about: "",
   };
-  const handleSubmit = (data) => {
+  const handleSubmit = (data, actions) => {
     console.log(data);
+    console.log(actions);
+    actions.resetForm();
   };
 
   return (
     <div>
-      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+      <Formik
+        validationSchema={schema}
+        initialValues={initialValues}
+        onSubmit={handleSubmit}>
         <Form>
           <label>
             <span>Name:</span>
             <Field name="name" />
+            <ErrorMessage name="name" />
           </label>
           <label>
             <span>Surname:</span>
             <Field name="surname" />
+            <ErrorMessage name="surname" />
           </label>
           <label>
             <span>Age:</span>
             <Field name="age" />
+            <ErrorMessage name="age" />
           </label>
           <label>
             <span>Email:</span>
             <Field name="email" />
+            <ErrorMessage name="email" />
           </label>
-          {/* <label>
+          <label>
             <span>Role</span>
-            <select>
+            <Field name="role" as="select">
               <option value="user">User</option>
               <option value="admin">Admin</option>
-            </select>
+            </Field>
+            <ErrorMessage name="role" />
           </label>
           <label>
             <span>About</span>
-            <textarea></textarea>
-          </label> */}
+            <Field as="textarea" name="about"></Field>
+            <ErrorMessage name="about" />
+          </label>
           <button type="submit">Create</button>
         </Form>
       </Formik>
